b5bc73d252d74777c90cda11031af88a8a7ab1b2
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2022-09-15 16:45:44 -0400

    Inital commit :rocket:

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+// import logo from './logo.svg';
+import '../App.css';
+import Footer from './Footer'
+
+const App = () =>  {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <h1>Working...</h1>
+      </header>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <div>
+            <div class="attribution">
+                <p>Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">Frontend Mentor</a>.</p>
+                <p>Coded by <a href="https://luztherose.github.io/portfolio/">Luz De La Rosa</a>.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
