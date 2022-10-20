05779f833419cc410ea4aae7ea0fdd51a6bb488a
Luz De La Rosa <luzdelarosaramirez@gmail.com>
2022-10-20 12:04:33 -0400

    Closes #26 Reply section

--- src/App.js
+++ src/App.js
@@ -1,27 +1,35 @@
-import { useState } from 'react';
-import Footer from './components/footer.jsx';
-import AddComment from './components/addComment.jsx';
-import Comment from './components/comment.jsx';
-import UserCommentReply from './components/userCommentReply'
-import data from "./data/data.json"
+import { useState, Fragment } from "react";
+import Footer from "./components/footer.jsx";
+import AddComment from "./components/addComment.jsx";
+import Comment from "./components/comment.jsx";
+import Reply from "./components/reply";
+import UserCommentReply from "./components/userCommentReply";
+import data from "./data/data.json";
 
-
-
-const App = () =>  {
-  const [comments, setComments] =useState(data.comments);
-  console.log(comments)
+const App = () => {
+  const [comments, setComments] = useState(data.comments);
+  console.log(comments);
   return (
     <div>
       <main className="bg-gray-100 p-12">
         {comments.map((comment, index) => {
-          return <Comment user="" comment={comment} key={index}/>
-        })} 
-        <UserCommentReply />
+          return (
+            <>
+              <Comment user="" comment={comment} key={comment.id} />
+              <section className="mt-8">
+                {comment.replies.map((reply) => {
+                  return <Reply user="" reply={reply} key={reply.id} />;
+                })}
+              </section>
+            </>
+          );
+        })}
+        {/* <UserCommentReply /> */}
         <AddComment />
       </main>
       <Footer />
     </div>
   );
-}
+};
 
 export default App;
